a61cc699b09c6eb21060879b4fbccb8088482b50
Pieterjan.Vl <pieterjan.vanlandeghem@student.hogent.be>
2024-09-18 16:44:12 +0200

    test 2

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,13 +11,12 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountnamepjaetest"
+  name                     = "storageaccountpjaetest"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  tags = {
-    environment = "local.default_tags"
-  }
+  tags = local.default_tags
+  
 }
